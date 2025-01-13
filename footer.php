b16242756fbc4cd73c271547175471d4c0a663c9
Aakash Pradhan <aakashpradhan1986@gmail.com>
2025-01-13 11:34:54 +0530

    Add header, footer, and initial home page layout

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container-fluid bg-dark text-light text-center py-3">
+        <p>Classified Ads &copy; <?php echo date('Y'); ?></p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="container-fluid bg-light">
+    <nav class="container navbar navbar-light bg-light">
+        <div class="logo">
+            <h1>Classified Ads</h1>
+        </div>
+        <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Listings</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Register</a></li>
+        </ul>
+    </nav>
+</header>
